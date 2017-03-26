f1c6337eca963c698b13549fbcaa86c3fd8fcbe9
Anna Springfield <anna.e.springfield@gmail.com>
2017-03-26 11:09:25 -0500

    add functions to update DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,15 +2,52 @@ $(document).ready(function() {
 
 });
 
-// listens for clicks on "Add task" button, stores task in database, and
+// listens for form submits through "Add task" button, stores task in database, and
 // refreshes task list
+function addTask() {
+  $('.taskForm').on('submit', function(e) {
+    e.preventDefault();
+    refreshTasks();
+  });
+}
 
 // POST request to store task in database
 
 // GET request to refresh task list
+function refreshTasks() {
+  $.ajax({
+    type: 'GET',
+    url: '/tasks/refresh',
+    success: function(response) {
+      console.log(response);
+      appendTasks(response);
+    }
+  });
+}
 
-// creates a div for a given task with the task description, a complete button,
-// and a delete button
+// takes an array of task objects with properties id, description, and complete
+// appends them to DOM
+function appendTasks(taskArray) {
+  $el = $(".taskContainer");
+  for (var i = 0; i < taskArray.length; i++) {
+    var taskDiv = createTaskDiv(taskArray[i]);
+    $el.append(taskDiv);
+  }
+}
+
+// takes a task object with properties id, description, and complete
+// creates a div with the task description, a complete button, and a delete button
+function createTaskDiv(task) {
+    var taskDesc = '<p>' + task.description + '</p>';
+    var completeButton = '<button class="completeButton">Complete</button>';
+    var deleteButton = '<button class="deleteButton">Delete</button>';
+    var dataAttrTaskId = 'data-taskId=' + task.id;
+    var dataAttrComplete = 'data-complete=' + task.complete;
+    var taskDiv ='<div class="task"' + dataAttrTaskId + dataAttrComplete + '>' +
+                  taskDesc + completeButton + deleteButton +
+                  '</div';
+    return taskDiv;
+}
 
 // listens for clicks on "complete" button and calls functions to:
 // change presentation of task
